80cbb1394a78e13e3add8563c6e7948b4bf39689
JadenMathias <mathias.jaden@gmail.com>
2021-08-05 16:30:25 +0400

    ads and optimization

---
+++ 3D Pong Project/Assets/Scripts/MenuAds.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+
+//Banner ad
+public class MenuAds : MonoBehaviour
+{
+	private BannerView adBanner;
+
+	private string idApp, idBanner;
+
+
+	void Start ()
+	{
+		idBanner = "ca-app-pub-3940256099942544/6300978111";
+
+		MobileAds.Initialize(initStatus => { });
+
+		RequestBannerAd ();
+	}
+
+
+
+	#region Banner Methods --------------------------------------------------
+
+	public void RequestBannerAd ()
+	{
+		adBanner = new BannerView (idBanner, AdSize.Banner, AdPosition.Bottom);
+		AdRequest request = AdRequestBuild ();
+		adBanner.LoadAd (request);
+	}
+
+	public void DestroyBannerAd ()
+	{
+		if (adBanner != null)
+			adBanner.Destroy();
+	}
+
+	#endregion
+
+	
+	//------------------------------------------------------------------------
+	AdRequest AdRequestBuild ()
+	{
+		return new AdRequest.Builder().Build();
+	}
+
+	void OnDestroy ()
+	{
+		DestroyBannerAd ();
+	}
+
+}
